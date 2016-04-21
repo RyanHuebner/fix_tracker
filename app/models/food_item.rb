c5cbf3feae8122039f52da238e8b6f69fb572202
Ryan Huebner <ryan.k.huebner@gmail.com>
2016-04-21 01:32:27 +0000

    added styling to food form.  Created shared error msg display.

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,6 +1,6 @@
 class FoodItem < ActiveRecord::Base
 
-    validates :name, presence: true, length: {minimum: 3, maximum: 20}
+    validates :name, presence: true, length: {minimum: 3, maximum: 40}
     validates :container_value, presence: true, 
         numericality: {greater_than_or_equal_to: 0}
     validates :container_color, presence: true    
